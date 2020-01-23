37b06d8f92a225c74489b6b7cfc2a01681dea35c
demmyhafiz <hafiz.janitra@gmail.com>
2020-01-23 17:54:31 +0700

    update on how to convert function constructor to ecmascript 2015 classes

--- main2.js
+++ main2.js
@@ -102,121 +102,173 @@
 // };
 
 
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
-const para = document.querySelector('p');
+// const input = document.querySelector('input');
+// const btn = document.querySelector('button');
+// const para = document.querySelector('p');
 
-btn.onclick = function () {
-    const code = input.value;
-    para.textContent = eval(code);
-}
+// btn.onclick = function () {
+//     const code = input.value;
+//     para.textContent = eval(code);
+// }
 
-function Person(first, last, age, gender, interests) {
-    this.name = {
+// function Person(first, last, age, gender, interests) {
+//     this.name = {
+//         first,
+//         last
+//     };
+//     this.age = age;
+//     this.gender = gender;
+//     this.interests = interests;
+// };
+
+// Person.prototype.bio = function () {
+//     // First define a string, and make it equal to the part of
+//     // the bio that we know will always be the same.
+//     let string = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ';
+//     // define a variable that will contain the pronoun part of
+//     // the second sentence
+//     let pronoun;
+
+//     // check what the value of gender is, and set pronoun
+//     // to an appropriate value in each case
+//     if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+//         pronoun = 'He likes ';
+//     } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+//         pronoun = 'She likes ';
+//     } else {
+//         pronoun = 'They like ';
+//     }
+
+//     // add the pronoun string on to the end of the main string
+//     string += pronoun;
+
+//     // use another conditional to structure the last part of the
+//     // second sentence depending on whether the number of interests
+//     // is 1, 2, or 3
+//     if (this.interests.length === 1) {
+//         string += this.interests[0] + '.';
+//     } else if (this.interests.length === 2) {
+//         string += this.interests[0] + ' and ' + this.interests[1] + '.';
+//     } else {
+//         // if there are more than 2 interests, we loop through them
+//         // all, adding each one to the main string followed by a comma,
+//         // except for the last one, which needs an and & a full stop
+//         for (let i = 0; i < this.interests.length; i++) {
+//             if (i === this.interests.length - 1) {
+//                 string += 'and ' + this.interests[i] + '.';
+//             } else {
+//                 string += this.interests[i] + ', ';
+//             }
+//         }
+//     }
+
+//     // finally, with the string built, we alert() it
+//     alert(string);
+// };
+
+// Person.prototype.greeting = function () {
+//     alert('Hi! I\'m ' + this.name.first + '.');
+// };
+
+// Person.prototype.farewell = function () {
+//     alert(this.name.first + ' has left the building. Bye for now!');
+// }
+
+// let person1 = new Person('Tammi', 'Smith', 17, 'female', ['music', 'skiing', 'kickboxing']);
+
+// function Teacher(first, last, age, gender, interests, subject) {
+//     Person.call(this, first, last, age, gender, interests);
+//     this.subject = subject;
+// }
+
+// Teacher.prototype = Object.create(Person.prototype);
+// Teacher.prototype.constructor = Teacher;
+
+// // This same as the above declaration
+// // Object.defineProperty(Teacher.prototype, 'constructor', {
+// //     value: Teacher,
+// //     enumerable: false, // so that it does not appear in 'for in' loop
+// //     writable: true
+// // });
+
+// Teacher.prototype.greeting = function () {
+//     let prefix;
+
+//     if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+//         prefix = 'Mr.';
+//     } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+//         prefix = 'Ms.';
+//     } else {
+//         prefix = 'Mx.';
+//     }
+
+//     alert('Hello. My name is ' + prefix + ' ' + this.name.last + ', and I teach ' + this.subject + '.');
+// };
+
+// let teacher1 = new Teacher('Dave', 'Griffiths', 31, 'male', ['football', 'cookery'], 'mathematics');
+
+// function Student(first, last, age, gender, interests) {
+//     Person.call(this, first, last, age, gender, interests);
+// }
+
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+
+// Student.prototype.greeting = function () {
+
+//     alert('Yo! I\'m ' + this.name.first + '.');
+// };
+
+// let student1 = new Student('Liz', 'Sheppard', 17, 'female', ['ninjitsu', 'air cadets']);
+
+
+// Basic Person class
+class Person {
+    constructor(first, last, age, gender, interests) {
+      this.name = {
         first,
         last
+      };
+      this.age = age;
+      this.gender = gender;
+      this.interests = interests;
+    }
+
+    greeting() {
+      console.log(`Hi! I'm ${this.name.first}`);
     };
-    this.age = age;
-    this.gender = gender;
-    this.interests = interests;
-};
 
-Person.prototype.bio = function () {
-    // First define a string, and make it equal to the part of
-    // the bio that we know will always be the same.
-    let string = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ';
-    // define a variable that will contain the pronoun part of
-    // the second sentence
-    let pronoun;
+    farewell() {
+      console.log(`${this.name.first} has left the building. Bye for now!`);
+    };
+  }
 
-    // check what the value of gender is, and set pronoun
-    // to an appropriate value in each case
-    if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
-        pronoun = 'He likes ';
-    } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
-        pronoun = 'She likes ';
-    } else {
-        pronoun = 'They like ';
+  // Instantiate Person
+
+  let han = new Person('Han', 'Solo', 25, 'male', ['Smuggling']);
+  han.greeting();
+  // Hi! I'm Han
+
+  let leia = new Person('Leia', 'Organa', 19, 'female' ['Government']);
+  leia.farewell();
+  // Leia has left the building. Bye for now
+
+  // Extend Person with Teacher
+
+  class Teacher extends Person {
+    constructor(first, last, age, gender, interests, subject, grade) {
+      super(first, last, age, gender, interests);
+
+      // subject and grade are specific to Teacher
+      this.subject = subject;
+      this.grade = grade;
     }
+  }
 
-    // add the pronoun string on to the end of the main string
-    string += pronoun;
+  // Instantiate Teacher
 
-    // use another conditional to structure the last part of the
-    // second sentence depending on whether the number of interests
-    // is 1, 2, or 3
-    if (this.interests.length === 1) {
-        string += this.interests[0] + '.';
-    } else if (this.interests.length === 2) {
-        string += this.interests[0] + ' and ' + this.interests[1] + '.';
-    } else {
-        // if there are more than 2 interests, we loop through them
-        // all, adding each one to the main string followed by a comma,
-        // except for the last one, which needs an and & a full stop
-        for (let i = 0; i < this.interests.length; i++) {
-            if (i === this.interests.length - 1) {
-                string += 'and ' + this.interests[i] + '.';
-            } else {
-                string += this.interests[i] + ', ';
-            }
-        }
-    }
-
-    // finally, with the string built, we alert() it
-    alert(string);
-};
-
-Person.prototype.greeting = function () {
-    alert('Hi! I\'m ' + this.name.first + '.');
-};
-
-Person.prototype.farewell = function () {
-    alert(this.name.first + ' has left the building. Bye for now!');
-}
-
-let person1 = new Person('Tammi', 'Smith', 17, 'female', ['music', 'skiing', 'kickboxing']);
-
-function Teacher(first, last, age, gender, interests, subject) {
-    Person.call(this, first, last, age, gender, interests);
-    this.subject = subject;
-}
-
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.constructor = Teacher;
-
-// This same as the above declaration
-// Object.defineProperty(Teacher.prototype, 'constructor', {
-//     value: Teacher,
-//     enumerable: false, // so that it does not appear in 'for in' loop
-//     writable: true
-// });
-
-Teacher.prototype.greeting = function () {
-    let prefix;
-
-    if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
-        prefix = 'Mr.';
-    } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
-        prefix = 'Ms.';
-    } else {
-        prefix = 'Mx.';
-    }
-
-    alert('Hello. My name is ' + prefix + ' ' + this.name.last + ', and I teach ' + this.subject + '.');
-};
-
-let teacher1 = new Teacher('Dave', 'Griffiths', 31, 'male', ['football', 'cookery'], 'mathematics');
-
-function Student(first, last, age, gender, interests) {
-    Person.call(this, first, last, age, gender, interests);
-}
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-
-Student.prototype.greeting = function () {
-
-    alert('Yo! I\'m ' + this.name.first + '.');
-};
-
-let student1 = new Student('Liz', 'Sheppard', 17, 'female', ['ninjitsu', 'air cadets']);
+  let snape = new Teacher('Severus', 'Snape', 58, 'male', ['Potions'], 'Dark arts', 5);
+  snape.greeting(); // Hi! I'm Severus.
+  snape.farewell(); // Severus has left the building. Bye for now.
+  console.log(snape.age) // 58
+  console.log(snape.subject) // Dark arts
