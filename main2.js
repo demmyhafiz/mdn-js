d1300f7a6d0c9bdf17818fa7d2794913fc86643b
demmyhafiz <hafiz.janitra@gmail.com>
2020-01-23 17:43:29 +0700

    fix comment

--- main2.js
+++ main2.js
@@ -1,109 +1,106 @@
-/*
-		function createNewPerson(name) {
-		const obj = {};
-		obj.name = name;
-		obj.greeting = function() {
-		alert('Hi! I\'m ' + obj.name + '.');
-		};
-		return obj;
-		}
-		*/
-/*
-function Person(name) {
-this.name = name;
-this.greeting = function() {
-alert('Hi! I\'m ' + this.name + '.');
-};
-}
+// How function works in JS
 
-let person1 = new Person('Bob');
-let person2 = new Person('Sarah');
-*/
-/*
-function Person(first, last, age, gender, interests) {
-this.name = {
-	first: first,
-	last: last
-	};
-this.age = age;
-this.gender = gender;
-this.interests = interests;
-this.bio = function() {
-	alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
-	};
-this.greeting = function() {
-	alert('Hi! I\'m ' + this.name.first + '.');
-	};
-}
+// 		function createNewPerson(name) {
+// 		const obj = {};
+// 		obj.name = name;
+// 		obj.greeting = function() {
+// 		alert('Hi! I\'m ' + obj.name + '.');
+// 		};
+// 		return obj;
+// 		}
 
-let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
-*/
+// function Person(name) {
+// this.name = name;
+// this.greeting = function() {
+// alert('Hi! I\'m ' + this.name + '.');
+// };
+// }
 
-/*
-function Person(first, last, age, gender, interests) {
-    this.name = {
-        first: first,
-        last: last
-    };
-    this.age = age;
-    this.gender = gender;
-    this.interests = interests;
-    this.bio = function () {
-        // First define a string, and make it equal to the part of
-        // the bio that we know will always be the same.
-        var string = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ';
-        // define a variable that will contain the pronoun part of
-        // the second sentence
-        var pronoun;
+// let person1 = new Person('Bob');
+// let person2 = new Person('Sarah');
 
-        // check what the value of gender is, and set pronoun
-        // to an appropriate value in each case
-        if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
-            pronoun = 'He likes ';
-        } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
-            pronoun = 'She likes ';
-        } else {
-            pronoun = 'They like ';
-        }
+// function Person(first, last, age, gender, interests) {
+// this.name = {
+// 	first: first,
+// 	last: last
+// 	};
+// this.age = age;
+// this.gender = gender;
+// this.interests = interests;
+// this.bio = function() {
+// 	alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+// 	};
+// this.greeting = function() {
+// 	alert('Hi! I\'m ' + this.name.first + '.');
+// 	};
+// }
 
-        // add the pronoun string on to the end of the main string
-        string += pronoun;
+// let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
 
-        // use another conditional to structure the last part of the
-        // second sentence depending on whether the number of interests
-        // is 1, 2, or 3
-        if (this.interests.length === 1) {
-            string += this.interests[0] + '.';
-        } else if (this.interests.length === 2) {
-            string += this.interests[0] + ' and ' + this.interests[1] + '.';
-        } else {
-            // if there are more than 2 interests, we loop through them
-            // all, adding each one to the main string followed by a comma,
-            // except for the last one, which needs an and & a full stop
-            for (var i = 0; i < this.interests.length; i++) {
-                if (i === this.interests.length - 1) {
-                    string += 'and ' + this.interests[i] + '.';
-                } else {
-                    string += this.interests[i] + ', ';
-                }
-            }
-        }
+// function Person(first, last, age, gender, interests) {
+//     this.name = {
+//         first: first,
+//         last: last
+//     };
+//     this.age = age;
+//     this.gender = gender;
+//     this.interests = interests;
+//     this.bio = function () {
+//         // First define a string, and make it equal to the part of
+//         // the bio that we know will always be the same.
+//         var string = this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. ';
+//         // define a variable that will contain the pronoun part of
+//         // the second sentence
+//         var pronoun;
 
-        // finally, with the string built, we alert() it
-        alert(string);
-    };
+//         // check what the value of gender is, and set pronoun
+//         // to an appropriate value in each case
+//         if (this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+//             pronoun = 'He likes ';
+//         } else if (this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+//             pronoun = 'She likes ';
+//         } else {
+//             pronoun = 'They like ';
+//         }
 
-    this.greeting = function () {
-        alert('Hi! I\'m ' + this.name.first + '.');
-    };
-};
+//         // add the pronoun string on to the end of the main string
+//         string += pronoun;
 
-let person1 = new Person('Tammi', 'Smith', 32, 'female', ['music', 'skiing', 'kickboxing', 'swimming', 'soccer']);
+//         // use another conditional to structure the last part of the
+//         // second sentence depending on whether the number of interests
+//         // is 1, 2, or 3
+//         if (this.interests.length === 1) {
+//             string += this.interests[0] + '.';
+//         } else if (this.interests.length === 2) {
+//             string += this.interests[0] + ' and ' + this.interests[1] + '.';
+//         } else {
+//             // if there are more than 2 interests, we loop through them
+//             // all, adding each one to the main string followed by a comma,
+//             // except for the last one, which needs an and & a full stop
+//             for (var i = 0; i < this.interests.length; i++) {
+//                 if (i === this.interests.length - 1) {
+//                     string += 'and ' + this.interests[i] + '.';
+//                 } else {
+//                     string += this.interests[i] + ', ';
+//                 }
+//             }
+//         }
 
-Person.prototype.farewell = function () {
-    alert(this.name.first + ' has left the building. Bye for now!');
-};
-*/
+//         // finally, with the string built, we alert() it
+//         alert(string);
+//     };
+
+//     this.greeting = function () {
+//         alert('Hi! I\'m ' + this.name.first + '.');
+//     };
+// };
+
+// let person1 = new Person('Tammi', 'Smith', 32, 'female', ['music', 'skiing', 'kickboxing', 'swimming', 'soccer']);
+
+// Person.prototype.farewell = function () {
+//     alert(this.name.first + ' has left the building. Bye for now!');
+// };
+
 
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
